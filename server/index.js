efd43f20a465004f9b4f9241e0be6179cbf11f14
Elena Spivak <spivak.e.v@gmail.com>
2021-03-27 21:20:57 +0300

    Add router + Use cors

--- server/index.js
+++ server/index.js
@@ -1,18 +1,17 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
-//const profileRouter = require('./routes/profile.routes')
 const models = require('./models/models')
-
+const cors = require('cors')
+const router = require('./routes/index')
 
 const PORT = process.env.PORT || 8080
 
 const app = express()
 
-/*
+app.use(cors())
 app.use(express.json())
-app.use('/api', profileRouter)
-*/
+app.use('/api', router)
 
 const start = async () => {
   try {
