cbef52bc4423c1dc8500f24f72f2f1a3f38608a2
Elena Spivak <spivak.e.v@gmail.com>
2021-03-28 14:10:38 +0300

    Change error message

--- server/middleware/errorHandlingMiddleware.js
+++ server/middleware/errorHandlingMiddleware.js
@@ -4,6 +4,6 @@ module.exports = function (err, req, res, next) {
   if (err instanceof ApiError) {
     return res.status(err.status).json({ message: err.message })
   }
-  return res.status(500).jon({message: "Непредвиденная ошибка!"})
+  return res.status(500).jon({message: "Unexpected error!"})
   // вызов next передаст управление след. в цепочке middleware
 }
