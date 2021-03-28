038e4a945a940ea33a981f9c373855cef05a07d8
Elena Spivak <spivak.e.v@gmail.com>
2021-03-28 14:10:15 +0300

    Change Bad Request status code + add notFound api error method

--- server/error/apiError.js
+++ server/error/apiError.js
@@ -6,15 +6,19 @@ class ApiError extends Error {
   }
 
   static badRequest(message) { //можно вызывать без создания объекта
+    return new ApiError(400, message)
+  }
+
+  static forbidden(message) {
+    return new ApiError(403, message)
+  }
+
+  static notFound(message) { //можно вызывать без создания объекта
     return new ApiError(404, message)
   }
 
   static internal(message) {
     return new ApiError(500, message)
-  }
-
-  static forbidden(message) {
-    return new ApiError(403, message)
   }
 }
 
