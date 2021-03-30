4c5c68146290a32e4be6950bf7ee00bc67378ba5
Elena Spivak <spivak.e.v@gmail.com>
2021-03-30 11:44:16 +0300

    Add goal router + controller

--- server/controllers/goalController.js
+++ server/controllers/goalController.js
@@ -1,24 +1,42 @@
-const db = require('../db')
+const { Goal } = require('../models/models')
+const ApiError = require('../error/apiError')
 
 class GoalController {
   async create(req, res) {
+    const { name, description, status, startDate, finishDate } = req.body
+    if (!name) return res.json(ApiError.badRequest("Goal 'name' is required"))
 
+    const goal = await Goal.create({ name, description, status, startDate, finishDate })
+    return res.json(goal)
   }
 
   async getAll(req, res) {
-
+    const goals = await Goal.findAll()
+    res.json(goals)
   }
 
   async getOne(req, res) {
-
+    const { id } = req.params
+    const goal = await Goal.findOne({ where: { id: id } })
+    res.json(goal)
   }
 
   async update(req, res) {
+    const { id, name, description, status, startDate, finishDate } = req.body
+    if (!id) return res.json(ApiError.badRequest("Goal 'id' is required"))
 
+    const goal = await Goal.update(
+      { name, description, status, startDate, finishDate },
+      { where: { id }, returning: true, plain: true }
+    )
+    return res.json(goal[1])
   }
 
   async delete(req, res) {
+    const { id } = req.params
 
+    const goal = await Goal.destroy({ where: { id } })
+    res.json(!!goal)
   }
 }
 
--- server/routes/goalRouter.js
+++ server/routes/goalRouter.js
@@ -1,10 +1,11 @@
 const Router = require('express')
 const router = new Router()
+const goalController = require('../controllers/goalController')
 
-router.post('/',)
-router.get('/:id',)
-router.get('/',)
-router.put('/',)
-router.delete('/:id',)
+router.post('/', goalController.create)
+router.get('/:id', goalController.getOne)
+router.get('/', goalController.getAll)
+router.put('/', goalController.update)
+router.delete('/:id', goalController.delete)
 
 module.exports = router
