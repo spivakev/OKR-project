05f43125d2aee9b6e9bda59d2a47c955448e1ea5
Elena Spivak <spivak.e.v@gmail.com>
2021-03-30 11:44:01 +0300

    Add department router + controller

--- server/controllers/departmentController.js
+++ server/controllers/departmentController.js
@@ -1,24 +1,42 @@
-const db = require('../db')
+const { Department } = require('../models/models')
+const ApiError = require('../error/apiError')
 
 class DepartmentController {
   async create(req, res) {
-    
+    const { name, description, email, city }
+    if (!name) return res.json(ApiError.badRequest("Department 'name' is required"))
+
+    const department = await Department.create({ name, description, email, city })
+    return res.json(department)
   }
 
   async getAll(req, res) {
-   
+    const departments = await Department.findAll()
+    res.json(departments)
   }
 
   async getOne(req, res) {
-    
+    const { id } = req.params
+    const department = Department.findOne({ where: { id: id } })
+    res.json(department)
   }
 
   async update(req, res) {
-   
+    const { id, name, description, email, city } = req.body
+    if (!id) return res.json(ApiError.badRequest("Department 'id' is required"))
+
+    const department = await Department.update(
+      { name, description, email, city },
+      { where: { id }, returning: true, plain: true }
+    )
+    return res.json(department[1])
   }
 
   async delete(req, res) {
-    
+    const { id } = req.params
+
+    const department = await Department.destroy({ where: { id } })
+    res.json(!!department)
   }
 }
 
--- server/routes/departmentRouter.js
+++ server/routes/departmentRouter.js
@@ -1,10 +1,11 @@
 const Router = require('express')
 const router = new Router()
+const departmentController = require('../controllers/departmentController')
 
-router.post('/',)
-router.get('/:id',)
-router.get('/',)
-router.put('/',)
-router.delete('/:id',)
+router.post('/', departmentController.create)
+router.get('/:id', departmentController.getOne)
+router.get('/', departmentController.getAll)
+router.put('/', departmentController.update)
+router.delete('/:id', departmentController.delete)
 
 module.exports = router
