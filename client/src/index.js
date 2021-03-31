5fcab2c3f9c07b63c3c67d01d4cb6201365dc44c
Elena Spivak <spivak.e.v@gmail.com>
2021-03-31 23:13:21 +0300

    Add redux and saga

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { compose, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import createSagaMiddleware from 'redux-saga'
+import { Provider } from "react-redux";
+import { rootReducer } from "./redux/reducers/rootReducer";
 import App from './App';
 
+const sagaMiddleware = createSagaMiddleware()
+//sagaMiddleware.run()
+
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk, sagaMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
+  )
+);
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
